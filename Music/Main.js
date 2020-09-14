2634b4d72870ed8ed4642ba364c1ad127090f402
thelittlecitizen12 <thelittlecitizen12@gmail.com>
2020-09-14 10:28:12 +0300

    Added instrument objects at Main.js

---
+++ Music/Main.js
@@ -0,0 +1,65 @@
+let flutelnstrument ={
+    description:'nice flute',
+    id: 1,
+    imagePath: 'halil.jpg',
+    instrumentOrder: null,
+    instrumentType: {id:1, name: 'blow' },
+    name: 'flute',
+    price: 200,
+    typeId:1
+}
+
+let guitarlnstrument ={
+    description:'nice guitar',
+    id: 2,
+    imagePath: 'guitar.png',
+    instrumentOrder: null,
+    instrumentType: {id:2, name: 'string' },
+    name: 'guitar',
+    price: 400,
+    typeId:2
+}
+
+let harmonicalnstrument ={
+    description:'nice harmonica',
+    id: 3,
+    imagePath: 'mapuhit.jpg',
+    instrumentOrder: null,
+    instrumentType: {id:3, name: 'blow' },
+    name: 'harmonica',
+    price: 1000,
+    typeId:3
+}
+
+let violinlnstrument ={
+    description:'nice violin',
+    id: 4,
+    imagePath: 'violin.jpg',
+    instrumentOrder: null,
+    instrumentType: {id:4, name: 'string' },
+    name: 'harmonica',
+    price: 1500,
+    typeId:4
+}
+
+let clarinetlnstrument ={
+    description:'nice clarinet',
+    id: 5,
+    imagePath: 'klarnit.jpg',
+    instrumentOrder: null,
+    instrumentType: {id:5, name: 'blow' },
+    name: 'clarinet',
+    price: 1700,
+    typeId:5
+}
+
+let trombonelnstrument ={
+    description:'nice trombone',
+    id: 6,
+    imagePath: 'trumbon.jpg',
+    instrumentOrder: null,
+    instrumentType: {id:6, name: 'blow' },
+    name: 'trombone',
+    price: 1700,
+    typeId:6
+}
